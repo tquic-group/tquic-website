561bd36481c27a05b369ec2819740bbeb7621d36
Sijie Yang <iyangsj@gmail.com>
2024-01-31 20:05:18 +0800

    Update website - update footer - update docs about installation and faq

---
+++ assets/js/037eda88.3b63ad19.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktquic_website=self.webpackChunktquic_website||[]).push([[8237],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ zh/assets/js/0d779e2c.1e3b6e83.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktquic_website=self.webpackChunktquic_website||[]).push([[1876],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
